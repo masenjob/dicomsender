2ff3ae08539fcbbbe762be2f0dcbc0b1b78ee9ea
masenjob <80282435+masenjob@users.noreply.github.com>
2021-10-19 13:19:20 -0300

    Update retry_failed.sh
    corrected destination job path

--- scripts/retry_failed.sh
+++ scripts/retry_failed.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Move failed jobs to the cmove in queue
+# 2021 Mauricio Asenjo
+# version 1
 
 QUEUEPATH=/cache/bqueue
 
-mv $QUEUEPATH/send2falp/failed/*.job $QUEUEPATH/verify/in
+mv $QUEUEPATH/send2falp/failed/*.job $QUEUEPATH/send2falp/out
 command rm $QUEUEPATH/send2falp/failed/*.log
 mv $QUEUEPATH/verify/failed/*.job $QUEUEPATH/cmove/in
 command rm $QUEUEPATH/verify/failed/*.log
